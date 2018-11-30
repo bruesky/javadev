c9789b42047914ba9928d219aaba6d174456a9ca
Xiangkui Guo <bruce1988cn@gmail.com>
2018-11-30 17:49:29 +0800

    OOPS Class Object Polymorphism Inheritance Abstraction Encapsulation

---
+++ hellojava/src/main/java/org/xkg/hellojava/oops/Hello.java
@@ -0,0 +1,29 @@
+package org.xkg.hellojava.oops;
+
+/*
+* Object Oriented Programming is a programming paradigm that consider programs as the collection of objects and data and
+* provides many concepts such as inheritance, abstraction, polymorphism,encapsulation,class and object.
+*
+* Class is a data structure that contains data and functions bundled together under a unit.It is a blueprint of specific object.
+* Class doesn't consume any space.
+*
+* Object is instance of class ,a runtime entity and it has its own state, behavior and identity.
+*
+* Binding method and data together into a single unit are known as encapsulation.A java class is the example of encapsulation.
+*
+* Hiding internal details and showing only essential method  is known as abstraction.
+* It helps to reduce the complexity and also improves the maintainability of the system.
+* In Java, we use abstract class and interface to achieve abstraction.
+*
+* Inheritance means to use something that already made.It is used in java for achieve concept of code reusability.
+*
+* Polymorphism is the ability to present the same interface for multiple forms.
+* In Java, we use method overloading and method overriding to achieve polymorphism.
+*
+* */
+
+
+
+public class Hello {
+
+}
