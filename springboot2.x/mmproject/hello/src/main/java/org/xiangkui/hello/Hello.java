8cb65eed51e1cb82da38cf610f8bab1f7025cec6
Xiangkui Guo <bruce1988cn@gmail.com>
2018-12-19 21:51:52 +0800

    multi modules template

---
+++ springboot2.x/mmproject/hello/src/main/java/org/xiangkui/hello/Hello.java
@@ -0,0 +1,7 @@
+package org.xiangkui.hello;
+
+public class Hello {
+    public static void main(String[]args){
+        org.xiangkui.libtest.Utils.test();
+    }
+}
---
+++ springboot2.x/mmproject/libtest/src/main/java/org/xiangkui/libtest/Utils.java
@@ -0,0 +1,7 @@
+package org.xiangkui.libtest;
+
+public class Utils {
+    public static void test(){
+        System.out.println("Test!!!");
+    }
+}
