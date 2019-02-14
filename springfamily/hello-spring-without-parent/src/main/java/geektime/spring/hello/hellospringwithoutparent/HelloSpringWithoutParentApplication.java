ac5a79a942c18bb24773971fa507ee5e78a4d549
Xiangkui Guo <bruce1988cn@gmail.com>
2019-02-14 14:47:40 +0800

    first class 2

---
+++ springfamily/hello-spring-without-parent/src/main/java/geektime/spring/hello/hellospringwithoutparent/HelloSpringWithoutParentApplication.java
@@ -0,0 +1,14 @@
+package geektime.spring.hello.hellospringwithoutparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringWithoutParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringWithoutParentApplication.class, args);
+	}
+
+}
+
