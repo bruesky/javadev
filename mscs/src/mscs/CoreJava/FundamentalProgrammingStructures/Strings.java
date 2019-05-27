3e1829d983e4a62f5464453cc4332ab3464ab969
Xiangkui Guo <bruce1988cn@gmail.com>
2019-05-27 17:05:24 -0500

    update ignore file

--- mscs/src/mscs/CoreJava/FundamentalProgrammingStructures/Strings.java
+++ mscs/src/mscs/CoreJava/FundamentalProgrammingStructures/Strings.java
@@ -4,7 +4,25 @@ import java.util.Scanner;
 
 public class Strings {
 
+    public static long repeatedString(String s, long n) {
+        long num1 = 0;
+        long num2 = 0;
+        char[] arr = s.toCharArray();
+        if (s.length()==1)
+            return arr[0]=='a'?n:0;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (arr[i]=='a')
+                num1++;
+            if (i<n%s.length()&&arr[i]=='a')
+                num2++;
+        }
+
+        return n/s.length()*num1+num2;
+    }
+
     public static void main(String[]args){
+        System.out.println(repeatedString("a",1000000000000l));
         String loc = "Java";
         String hello = "Hello";
         String greeting = hello+" "+loc;
@@ -21,5 +39,11 @@ public class Strings {
         }
         System.out.println(stringBuilder.toString());
         System.out.println("world".equalsIgnoreCase("WORLD"));
+
+        int[] codePoints = greeting.codePoints().toArray();
+        for (int c:codePoints
+             ) {
+            System.out.println(c);
+        }
     }
 }
