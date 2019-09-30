c738d83d6940cbd94c70ac9e31f070493aee7850
Xiangkui Guo <bruce1988cn@gmail.com>
2019-09-30 18:09:35 -0500

    lab1

---
+++ waa/lab1/src/main/java/edu/mum/listener/StarbucksContextListener.java
@@ -0,0 +1,33 @@
+package edu.mum.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import edu.mum.data.DataFacade;
+import edu.mum.data.DataFacadeFactory;
+
+
+@WebListener
+public class StarbucksContextListener implements ServletContextListener{
+
+		  public void contextInitialized(ServletContextEvent event){
+ 
+			ServletContext servletContext = event.getServletContext();
+
+			// Set up Database access
+			String version = servletContext.getInitParameter("version");
+ 			DataFacade data = (new DataFacadeFactory()).getDataFacadeInstance(version);
+
+		    servletContext.setAttribute("dataSource", data);
+
+ 		  }
+
+		  public void contextDestroyed(ServletContextEvent event){}
+
+ 
+
+
+
+}
