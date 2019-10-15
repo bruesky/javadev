02894dcb02f43e366279c47097aa9806da91d71a
Xiangkui Guo <bruce1988cn@gmail.com>
2019-10-15 16:37:42 -0500

    lab11

---
+++ waa/lab11/src/main/java/edu/mum/lab11/service/UserService.java
@@ -0,0 +1,41 @@
+package edu.mum.lab11.service;
+
+
+import edu.mum.lab11.model.Role;
+import edu.mum.lab11.model.User;
+import edu.mum.lab11.repository.RoleRepository;
+import edu.mum.lab11.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+}
