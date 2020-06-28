07982854543a7478eb6d34f3cebc76c27cc68f05
Xiangkui Guo <bruce1988cn@gmail.com>
2020-06-28 16:43:49 -0500

    init interviewj project

---
+++ interviewj/src/test/java/org/saki/interviewj/InterviewjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saki.interviewj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
